03f7bad6bfc984b466594eac2f8b1fb2f1e05a69
Tinku Rajput <112786910+tinku-258@users.noreply.github.com>
2023-04-24 16:23:49 +0530

    Update Depth of BT.cpp

--- Binary Tree/Depth of BT.cpp
+++ Binary Tree/Depth of BT.cpp
@@ -4,3 +4,29 @@
         }
         return 0;
  }
+
+
+---------------ITERATIVE---------------------------------------------------------------------
+ 
+int maxDepth(TreeNode* root) {
+        if(root==NULL) return 0;
+         queue<TreeNode*> q;
+         q.push(root);
+         int ans=0;
+         while(!q.empty()){
+             int s=q.size();
+             while(s--){
+                 TreeNode* node=q.front();
+                 q.pop();
+
+                 if(node->left){
+                     q.push(node->left);
+                 }
+                 if(node->right){
+                     q.push(node->right);
+                 }
+             }
+             ans++;
+         }
+         return ans;
+}
