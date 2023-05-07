6ae6325600cb3a8c43f0723c09b9c8d3101fb43b
Tinku Rajput <112786910+tinku-258@users.noreply.github.com>
2023-05-07 18:38:50 +0530

    Create Modular Power.cpp

---
+++ Math/Modular Power.cpp
@@ -0,0 +1,10 @@
+// returns power(x,y) with mode m
+
+int modPow(long long x, long long y, int m)
+ {
+      if (y == 0)
+        return 1;
+      long long p = modPow(x, y / 2, m);
+      p = (p * p) % m;
+      return y % 2 ? (p * (x % m)) % m : p;
+ }
