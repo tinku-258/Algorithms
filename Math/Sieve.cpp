379e5e8fdaa8915d9740f3b6584486710125896c
Tinku Rajput <112786910+tinku-258@users.noreply.github.com>
2023-05-28 07:37:21 +0530

    Update Sieve.cpp

--- Math/Sieve.cpp
+++ Math/Sieve.cpp
@@ -1,24 +1,26 @@
 // returns the list of prime numbers upto N
 
-vector<int> sieve(int N)
+ vector<int> sieve(int N)
     {
         vector<bool> prime(N+1,true);
         vector<int> result;
         int p=2;
         while(p*p<=N)
-        {
+        {    
+            if(prime[p]==true){
+             result.push_back(p);
               for(int i=2;p*i<=N;i++)
               {
                   prime[p*i]=false;
-              }
-              p++;
-              while(prime[p]==false)
-                p++;
+              }  
+            }
+             p++;
         }
-        for(int i=2;i<=N;i++)
-        {
-            if(prime[i]==true)
-              result.push_back(i);
+        while(p<=N){
+            if(prime[p]==true){
+                result.push_back(p);
+            }
+            p++;
         }
         return result;
     }
