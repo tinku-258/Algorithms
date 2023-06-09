b62d29065aee5d05392af3714db3cc89a1bc8fe1
Tinku Rajput <112786910+tinku-258@users.noreply.github.com>
2023-06-09 14:28:49 +0530

    Update MakeGraph.cpp

--- Graph/MakeGraph.cpp
+++ Graph/MakeGraph.cpp
@@ -4,3 +4,10 @@
            adj[e[0]].push_back({e[1],e[2]});
            adj[e[1]].push_back({e[0],e[2]});
        }
+
+// Un Weighted graph
+      vector<vector<int>> adj(n+1);
+       for(auto& e:edges){
+           adj[e[0]].push_back(e[1]);
+           adj[e[1]].push_back(e[0]);
+       }
